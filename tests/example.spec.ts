cec4b1162afdad20ec8eed51d586122999ee65cf
leuneri <ericleung147258369@hotmail.com>
2025-05-30 14:23:38 -0800

    switched from goodnotes to whiteboard as substitute due to verification issues with goodnotes external source logins, happy case

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,26 +1,26 @@
 import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
+dotenv.config()
 
-test('has title', async ({ page }) => {
-  await page.goto('https://www.goodnotes.com/web');
+  test('Go into Whiteboard link and make new whiteboard', async ({ page }) => {
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('Goodnotes for Web | Free to Download');
-});
+    const username = process.env.GOOGLE_USERNAME;
+    const password = process.env.GOOGLE_PASSWORD;
+    await page.goto('https://www.whiteboard.team/');
 
-test('Try Goodnotes for Web link', async ({ page }) => {
-  await page.goto('https://www.goodnotes.com/web');
-
-  // Click the 'Try Goodnotes for Web' link.
-  const [notebookPage] = await Promise.all([
-    page.context().waitForEvent('page'), // Wait for the new tab to open
-    page.locator('.web-buttons-flexbox-div', { hasText: 'Try Goodnotes for Web' }).click()
-  ]);
-
-  await notebookPage.waitForLoadState();
-
-  await expect(notebookPage).toHaveURL('https://web.goodnotes.com/home');
-  // need to sign in
-
+    await page.waitForLoadState();
+    // need to sign in
+    await page.locator('#authButton').click(),
+    await page.waitForLoadState();
+    await expect(page.locator('input[placeholder="email"]')).toBeVisible();
+    await expect(page.locator('input[placeholder="Password"]')).toBeVisible();
+    await page.locator('input[placeholder="email"]').fill(username!);
+    await page.locator('input[placeholder="Password"]').fill(password!);
+    await page.getByRole('button', { name: 'Log in' }).click();
+    
+    await page.waitForLoadState();
+    await expect(page).toHaveTitle('Whiteboard Team')
+    await page.getByRole('button', { name: 'New' }).click();
   // TODO: setup where we create a new notebook every time
-  await expect(page.getByTestId('Untitled Notebook-libraryThumbnailImage')).toBeVisible();
+  // await expect(signInPopup.getByTestId('Untitled Notebook-libraryThumbnailImage')).toBeVisible();
 });
